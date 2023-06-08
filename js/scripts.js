4eb3f21d6d9f4a4f7d3e16a33e87949fdc0ba1b8
Gene Courtney <genecourtney@Genes-MacBook-Pro.local>
2023-06-07 21:29:12 -0500

    Adding JS code to scripts.js. Actually does addition in prompts.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,16 @@ function add (number1,  number2){
     return number1 + number2;
 }
 
-saySomething("hi");
-const result  = add(3, 5);
-window.alert(result);
+const result = add(3, 5);
+const outputText = "The sum is " + result + ".";
+
+//business logic
+function add (number1, number2)  {
+    return number1  + number2;
+}
+
+//user interface logic
+const number1 = parseInt(prompt("Enter a Number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+
+window.alert(add(number1, number2));
